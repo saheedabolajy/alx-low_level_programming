72a43fb56558b2e98321098b8f4aee2fea13cefa
saheedabolajy <saheedolatona@gmail.com>
2022-12-11 22:09:51 -0800

    task-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
 
 int main(void)
 {
-     puts("\" programming is like building a multi lingual puzzle");
-     return ();
+     puts("\" programming is like building a multi lingual puzzle);
+     return (0);
 }
