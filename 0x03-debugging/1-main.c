ad725cae6d7cea52664a6a7064ff06ccef41d94f
saheedabolajy <saheedolatona@gmail.com>
2022-12-15 20:42:53 -0800

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinte loop
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+printf("infintr long incoming :(\n");
+
+i = 0
+/*
+* while (i < 10)
+* {
+ 	putchar(i);
+*}
+*/
+printf("infinte loop avoided: \\0/\n");
+
+return (0);
+}
