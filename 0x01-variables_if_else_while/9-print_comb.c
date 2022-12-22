310e2c59e2cbb6eaf700db2fff75a2daa592a459
saheedabolajy <saheedolatona@gmail.com>
2022-12-22 01:08:31 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination of single digit
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
