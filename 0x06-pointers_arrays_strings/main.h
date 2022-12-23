8405b5946b28c119e9d46a95ba5da5e0ec7255ae
saheedabolajy <saheedolatona@gmail.com>
2022-12-22 20:41:59 -0800

    task 8

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -20,5 +20,13 @@ char *leet(char *a);
 
 int _putchar(char c);
 
+char *rot13(char *);
+
+void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
+
 
 #endif /* MAIN_H */
