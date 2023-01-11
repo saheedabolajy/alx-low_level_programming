f2a1ac3c60bf42cf93db5c809bd666cd3ce02cb0
saheedabolajy <saheedolatona@gmail.com>
2023-01-11 15:21:30 -0800

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,17 +16,22 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
+
 	for (i = 0; str[i] != '\0'; i++)
-		;
+	;
 	i++;
+
 	if (i < 1)
 		return (NULL);
+
 	p = malloc(sizeof(char) * 1);
+
 	if (p == NULL)
 	{
 		free(p);
 		return (NULL);
 	}
+
 	for (j = 0; j < i; j++)
 		p[j] = str[j];
 	p[j] = '\0';
