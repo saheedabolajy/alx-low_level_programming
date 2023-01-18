f109e5e934f28faea5b11aff5e4b03e216da68f6
saheedabolajy <saheedolatona@gmail.com>
2023-01-17 18:00:10 -0800

    task_4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,6 +46,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		n_dog->name[i] = name[i];
 	for (i = 0; i <= lenO; i++)
 		n_dog->owner[i] = owner[i];
-	n_dog -> age = age;
+	n_dog->age = age;
 	return (n_dog);
 }
